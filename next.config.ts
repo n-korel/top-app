973e9bf6adb61a49c10c597f9c8dea4128748dd7
n-korel <n.korelskiy@hotmail.com>
2024-11-28 18:14:35 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,33 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-	webpack: (config) => {
+	webpack(config) {
 		config.module.rules.push({
 			test: /\.svg$/,
-			use: ['@svgr/webpack'],
+			use: [
+				{
+					loader: '@svgr/webpack',
+					options: {
+						svgo: true,
+						svgoConfig: {
+							plugins: [
+								{
+									name: 'preset-default',
+									params: {
+										override: {
+											removeViewBox: false, // Сохранить атрибут viewBox
+										},
+									},
+								},
+							],
+						},
+						prettier: false, // Отключить форматирование
+						titleProp: true, // Добавить title как проп
+					},
+				},
+			],
 		});
+
 		return config;
 	},
 };
