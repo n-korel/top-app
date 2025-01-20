87447576b0981fac7661963eaf273a6a9eed47de
n-korel <n.korelskiy@hotmail.com>
2025-01-20 15:00:50 +0300

    getStaticProps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,11 @@ import { P } from './components/P/P';
 import { Tag } from './components/Tag/Tag';
 import { Rating } from './components/Rating/Rating';
 import { withLayout } from './layout/Layout';
+import { GetStaticProps } from 'next';
+import axios from 'axios';
+import { MenuItem } from './interfaces/menu.interface';
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
 	const [rating, setRating] = useState<number>(4);
 
 	return (
@@ -32,8 +35,32 @@ function Home(): JSX.Element {
 			</Tag>
 			<Tag color="primary">Primary</Tag>
 			<Rating rating={rating} isEditable setRating={setRating} />
+			<ul>
+				{menu.map((m) => (
+					<li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+				))}
+			</ul>
 		</>
 	);
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+	const firstCategory = 0;
+	const { data: menu } = await axios.post<MenuItem[]>(
+		process.env.NEXT_PUBLIC_DOMAIN + 'api/top-page/find',
+		{ firstCategory },
+	);
+	return {
+		props: {
+			menu,
+			firstCategory,
+		},
+	};
+};
+
+interface HomeProps extends Record<string, unknown> {
+	menu: MenuItem[];
+	firstCategory: number;
+}
