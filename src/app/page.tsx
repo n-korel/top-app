3ad5a28218fd86712d83eda393de5bc7a453dfe3
n-korel <n.korelskiy@hotmail.com>
2025-01-23 13:42:15 +0300

    Static generation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,30 @@
 'use client';
 
-import { useState } from 'react';
-import { Button } from './components/Button/Button';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Htag } from './components/Htag/Htag';
+import { Button } from './components/Button/Button';
 import { P } from './components/P/P';
 import { Tag } from './components/Tag/Tag';
 import { Rating } from './components/Rating/Rating';
+import { MenuItem } from './interfaces/menu.interface';
 import { withLayout } from './layout/Layout';
 
 function Home(): JSX.Element {
-	const [rating, setRating] = useState<number>(4);
+	const [menu, setMenu] = useState<MenuItem[]>([]);
+	const firstCategory = 0;
+
+	useEffect(() => {
+		const fetchMenu = async () => {
+			const { data } = await axios.post<MenuItem[]>(
+				`${process.env.NEXT_PUBLIC_DOMAIN}api/top-page/find`,
+				{ firstCategory },
+			);
+			setMenu(data);
+		};
+
+		fetchMenu();
+	}, []);
 
 	return (
 		<>
@@ -31,7 +46,12 @@ function Home(): JSX.Element {
 				Green
 			</Tag>
 			<Tag color="primary">Primary</Tag>
-			<Rating rating={rating} isEditable setRating={setRating} />
+			<Rating rating={4} isEditable setRating={() => {}} />
+			<ul>
+				{menu.map((m) => (
+					<li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+				))}
+			</ul>
 		</>
 	);
 }
